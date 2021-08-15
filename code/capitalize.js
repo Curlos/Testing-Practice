f4b2473e4940c0a183a8adc07378022c73071ef1
Carlos Martinez <curlosmart@gmail.com>
2021-08-15 09:37:05 -0400

    Capitalize (complete)

--- code/capitalize.js
+++ code/capitalize.js
@@ -1,5 +1,14 @@
-const capitalize = () => {
+const capitalize = (string) => {
+  if (string.length == 0 || string == null) {
+    return string
+  }
+  const capitalFirstLetter = string.slice(0, 1).toUpperCase()
+  const restOfStr = string.slice(1)
+  
+  return capitalFirstLetter + restOfStr
 
 }
+
+console.log(capitalize('ERREJkdsfjsjfR'))
 
 module.exports = capitalize;
