c325ccb718c4c6151d88bd8cdab38cdf16e4c29c
Carlos Martinez <curlosmart@gmail.com>
2021-08-15 09:36:56 -0400

    Array analysis (complete)

--- code/arrayAnalysis.js
+++ code/arrayAnalysis.js
@@ -1,5 +1,16 @@
-const arrayAnalysis = () => {
+const arrayAnalysis = (arrOfNum) => {
+  const sum = arrOfNum.reduce((accumulator, currentValue) => accumulator + currentValue)
+  const average = sum / arrOfNum.length
+  const arrProps = {
+    average,
+    min: Math.min(...arrOfNum),
+    max: Math.max(...arrOfNum),
+    length: arrOfNum.length,
+  }
 
+  return arrProps
 };
+
+console.log(arrayAnalysis([1,8,3,4,2,6]))
 
 module.exports = arrayAnalysis;
