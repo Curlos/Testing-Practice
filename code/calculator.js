ac42033f75af50449246069c988301b2608a0f3b
Carlos Martinez <curlosmart@gmail.com>
2021-08-15 09:51:10 -0400

    Calculator (complete)

--- code/calculator.js
+++ code/calculator.js
@@ -1,5 +1,29 @@
-const calculator = () => {
+const add = (...arguments) => {
+  return arguments.reduce((accumulator, currentValue) => accumulator + currentValue)
+}
 
+const subtract = (...arguments) => {
+  return arguments.reduce((accumulator, currentValue) => accumulator - currentValue)
+}
+
+const multiply = (...arguments) => {
+  return arguments.reduce((accumulator, currentValue) => accumulator * currentValue)
+}
+
+const divide = (...arguments) => {
+  return arguments.reduce((accumulator, currentValue) => accumulator / currentValue)
+}
+
+const calculator = {
+  'add': add,
+  'subtract': subtract,
+  'multiply': multiply,
+  'divide': divide,
 };
+
+console.log(calculator.add(1,2,3,4))
+console.log(calculator.subtract(10,3))
+console.log(calculator.multiply(10,3))
+console.log(calculator.divide(10,3,2))
 
 module.exports = calculator;
